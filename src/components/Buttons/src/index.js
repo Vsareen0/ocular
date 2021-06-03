bbbc2104eb0ae2e93a52ab0e244a51182e01e0ca
Vinamra Sareen <vsareen24@gmail.com>
2021-06-03 15:26:30 +0530

    Added babel compilation

---
+++ src/components/Buttons/src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _Buttons.Button;
+  }
+});
+
+var _Buttons = require("./Buttons");
